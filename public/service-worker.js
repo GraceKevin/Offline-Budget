0df9a24b1e41c632bf6c922ce57a79b1748a8f8f
Kevin Grace <kgrace90@yahoo.com>
2021-10-31 19:57:09 -0400

    more properly adjusted service-worker

--- public/service-worker.js
+++ public/service-worker.js
@@ -1,10 +1,12 @@
 // Module 19.4.5 assist in creating js file - (UCF)
 
-const APP_PREFIX = 'Tracker-';     
+// const APP_PREFIX = 'Tracker-';     
 
-const VERSION = 'version_01';
+// const VERSION = 'version_01';
 
-const CACHE_NAME = APP_PREFIX + VERSION;
+const CACHE_NAME = 'budget-cache-v1';
+
+const DATA_CACHE_NAME = 'data-cache-v1';
 
 const FILES_TO_CACHE = [
 
@@ -29,7 +31,7 @@ const FILES_TO_CACHE = [
 self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open(CACHE_NAME).then(cache => {
-            console.log('installing cache : ' + CACHE_NAME);
+            console.log('Cached files successfully.');
             return cache.addAll(FILES_TO_CACHE);
         })
     );
@@ -40,19 +42,13 @@ self.addEventListener('install', function(event) {
 
 self.addEventListener('activate', function (event) {
   event.waitUntil(
-    caches.keys().then(function (keyList) {
-      let cacheKeeplist = keyList.filter(function (key) {
-        return key.indexOf(APP_PREFIX);
-      })
-
-      cacheKeeplist.push(CACHE_NAME);
-
-      return Promise.all(keyList.map(function (key, i) {
-        if (cacheKeeplist.indexOf(key) === -1) {
-          console.log('deleting cache : ' + keyList[i] );
-          return caches.delete(keyList[i]);
+    caches.keys().then(keylist => {
+      return Promise.all(key => {
+        if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
+          console.log('Deleting old cache', key);
+          return caches.delete(key);
         }
-      }));
+      });
     })
   );
   self.clients.claim();
@@ -60,20 +56,37 @@ self.addEventListener('activate', function (event) {
 
 // Fetch request
 
-self.addEventListener('fetch', function (event) {
-    console.log('fetch request : ' + event.request.url)
+self.addEventListener('fetch', function(event) {
+    if (event.request.url.includes('/api/')) {
+      event.respondWith(
+        caches
+          .open(DATA_CACHE_NAME)
+          .then(cache => {
+            return fetch(event.request)
+              .then(response => {
+                if (response.status === 200) {
+                  cache.put(event.request.url, response.clone());
+                }
+                return response;
+              })
+              .catch(err => {
+                return cache.match(event.request);
+              });
+          })
+          .catch(err => console.log(err))
+      );
+      return;
+    }
+  
     event.respondWith(
-      caches.match(e.request).then(function (request) {
-        if (request) { 
-            // if cache is available, respond with cache
-          console.log('responding with cache : ' + event.request.url)
-          return request
-        } else {       
-            // if there are no cache, try fetching request
-          console.log('file is not cached, fetching : ' + event.request.url)
-          return fetch(event.request)
-        }
-
+      fetch(event.request).catch(function() {
+        return caches.match(event.request).then(function(response) {
+          if (response) {
+            return response;
+          } else if (event.request.headers.get('accept').includes('text/html')) {
+            return caches.match('/');
+          }
+        });
       })
-    )
-  }) 
+    );
+});
